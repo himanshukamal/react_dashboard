bd69f4acc3d877e388798941c79116ab3c067625
himanshukamal <himanshu.55421@gmail.com>
2021-07-08 11:52:57 +0530

    updates article layout

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Articles from './component/articles';
 function App() {
   return (
     <div className="App">
-      <ReadingMenu />
-      {/* <Articles /> */}
+      {/* <ReadingMenu /> */}
+      <Articles />
       
     </div>
   );
--- src/component/articles.js
+++ src/component/articles.js
@@ -17,7 +17,7 @@ class Articles extends Component {
                     </div>
                     <div className="title_and_art">
                         <p className="article_title">
-                            we are all confident idiots
+                            we are all confident idiots.lorem ipsum loewm ipaium ploowm iapuanm oloewn loinsdnsds
                         </p>
                         <div className="article_art">
                             <img src="./avatar1.png" />
@@ -28,7 +28,81 @@ class Articles extends Component {
                     </div>
 
                 </div>
+                <div className="margin_top">
+                    <div className="first_article">
+                        <div className="image_and_name">
+                            <div className="image_wrapper">
+                                <img src="./avatar1.png" />
+                            </div>
+                            <div className="author_name">
+                                Ella Alderson
+                            </div>
+                        </div>
+                        <div className="title_and_art">
+                            <p className="article_title">
+                                we are all confident idiots.lorem ipsum loewm ipaium ploowm iapuanm oloewn loinsdnsds
+                            </p>
+                            <div className="article_art">
+                                <img src="./avatar1.png" />
+                            </div>
+                        </div>
+                        <div className="date_and_time">
+                            4 mins ago
+                        </div>
+                    </div>
+                </div>
+
+                <div className="margin_top">
+                    <div className="first_article">
+                        <div className="image_and_name">
+                            <div className="image_wrapper">
+                                <img src="./avatar1.png" />
+                            </div>
+                            <div className="author_name">
+                                Ella Alderson
+                            </div>
+                        </div>
+                        <div className="title_and_art">
+                            <p className="article_title">
+                                we are all confident idiots.lorem ipsum loewm ipaium ploowm iapuanm oloewn loinsdnsds
+                            </p>
+                            <div className="article_art">
+                                <img src="./avatar1.png" />
+                            </div>
+                        </div>
+                        <div className="date_and_time">
+                            4 mins ago
+                        </div>
+                    </div>
+                </div>
+
+                <div className="margin_top">
+                    <div className="first_article">
+                        <div className="image_and_name">
+                            <div className="image_wrapper">
+                                <img src="./avatar1.png" />
+                            </div>
+                            <div className="author_name">
+                                Ella Alderson
+                            </div>
+                        </div>
+                        <div className="title_and_art">
+                            <p className="article_title">
+                                we are all confident idiots.lorem ipsum loewm ipaium ploowm iapuanm oloewn loinsdnsds
+                            </p>
+                            <div className="article_art">
+                                <img src="./avatar1.png" />
+                            </div>
+                        </div>
+                        <div className="date_and_time">
+                            4 mins ago
+                        </div>
+                    </div>
+                </div>
             </div>
+
+            
+            
             
             )
         }
